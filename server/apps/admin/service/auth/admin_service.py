097264d5ab5a6c4208051e583b53ce2f52186e20
zero <10652944+wafts@user.noreply.gitee.com>
2024-09-20 11:21:25 +0800

    update: 调整描述

--- server/apps/admin/service/auth/admin_service.py
+++ server/apps/admin/service/auth/admin_service.py
@@ -3,8 +3,8 @@
 # +----------------------------------------------------------------------
 # | 欢迎阅读学习程序代码,建议反馈是我们前进的动力
 # | 程序完全开源可支持商用,允许去除界面版权信息
-# | gitee:   https://gitee.com/wafts/waitadmin_fastapi
-# | github:  https://github.com/topwait/waitadmin_fastapi
+# | gitee:   https://gitee.com/wafts/waitadmin-python
+# | github:  https://github.com/topwait/waitadmin-python
 # | 官方网站: https://www.waitadmin.cn
 # | WaitAdmin团队版权所有并拥有最终解释权
 # +----------------------------------------------------------------------
