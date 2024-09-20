7c296de70abbe439fc5fa271e9b484a2f75e87bc
zero <10652944+wafts@user.noreply.gitee.com>
2024-09-20 17:11:38 +0800

    update: 前台端接入统计脚本

--- nuxtjs/app.vue
+++ nuxtjs/app.vue
@@ -14,6 +14,7 @@ import useConfStore from './stores/conf'
 const appStore = useAppStore()
 const confStore = useConfStore()
 const pcConfig = appStore.getPcConfig
+const websiteConfig = appStore.getWebsiteConfig
 
 const elConfig = {
     zIndex: 3000,
@@ -47,6 +48,7 @@ useHead({
             name: 'description',
             content: pcConfig.description
         }
-    ]
+    ],
+    script: websiteConfig.analyse ? [{src: websiteConfig.analyse}] : []
 })
 </script>
