73a2ce01462867e181994a6490f461f14718737a
zero <10652944+wafts@user.noreply.gitee.com>
2024-11-18 15:36:24 +0800

    优化验证码类注释

--- server/plugins/captcha/src.py
+++ server/plugins/captcha/src.py
@@ -25,10 +25,24 @@ background_color = [(255, 255, 255), (211, 211, 211), (245, 245, 245)]
 
 
 def color():
+    """
+    生成一个随机的RGB颜色值。
+    """
     return tuple((random.randint(0, 255) for _ in range(3)))
 
 
 def get_xy(width, height):
+    """
+    生成四个随机的x, y坐标值，用于验证码字符的定位。
+
+    Args:
+        width (int): 图片的宽度。
+        height (int): 图片的高度。
+
+    Returns:
+        List[int]: 包含四个整数的列表，分别代表两个字符的随机x, y坐标。
+                   列表格式为 [x1, y1, x2, y2]，其中(x1, y1)和(x2, y2)是两个字符的坐标。
+    """
     return [
         random.randint(int(width / 2), int(width)),
         random.randint(int(height / 2), int(height)),
@@ -40,8 +54,12 @@ def get_xy(width, height):
 def captcha(num: int = 4) -> str:
     """
     生成随机验证码
-    :param num: 验证码位数
-    :return: 文本验证码
+    
+    Args:
+        num (int): 验证码位数。
+    
+    Returns: 
+        (str): 文本验证码
     """
     return "".join(
         [random.choice(string.digits + string.ascii_letters) for _ in range(num)]
@@ -56,13 +74,17 @@ def img_captcha(
     byte_stream: bool = False,
 ) -> Tuple[Union[Image.Image, BytesIO], str]:
     """
-    生成图形验证码，返回Image对象 , 验证码文本
-    :param width: 验证码长度（x轴） 默认 150
-    :param height: 图片宽度（y轴） 默认 40
-    :param font_size: 字体大小 默认 18
-    :param code_num: 验证码位数 默认 4
-    :param byte_stream: byte io 流的形式返回
-    :return: (Image, str)
+    生成图形验证码, 返回Image对象, 验证码文本
+
+    Args:
+        width (int): 验证码长度（x轴） 默认 150。
+        height (int): 图片宽度（y轴） 默认 40。
+        font_size (int): 字体大小 默认 18。
+        code_num (int): 验证码位数 默认 4。
+        byte_stream (int): byte io 流的形式返回。
+
+    Returns: 
+        (Image, str)
     """
     # 创建图形
     img = Image.new("RGB", (width, height), random.choice(background_color))
@@ -96,8 +118,25 @@ def img_captcha(
 
 
 def b64_captcha(
-    width: int = 150, height: int = 60, font_size: int = 39, code_num: int = 4
+        width: int = 150,
+        height: int = 60,
+        font_size: int = 39,
+        code_num: int = 4
 ) -> Tuple[str, str]:
+    """
+    生成一个指定宽度、高度、字体大小和字符数量的验证码图片，
+    并将图片转换为base64编码的字符串格式, 同时返回验证码文本。
+
+    Args:
+        width (int): 图片的宽度, 默认为150像素。
+        height (int): 图片的高度, 默认为60像素。
+        font_size (int): 验证码中字符的字体大小, 默认为39像素。
+        code_num (int): 验证码中字符的数量, 默认为4个字符。
+
+    Returns:
+        Tuple[str, str]: 一个元组, 第一个元素是base64编码的图片字符串,
+                       第二个元素是验证码文本字符串。
+    """
     img, code = img_captcha(width, height, font_size, code_num, byte_stream=True)
     b64_img = "data:image/jpeg;base64," + b64encode(img.getvalue()).decode(
         encoding="utf-8"
