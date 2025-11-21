3fccc5a4eee2201e2d78ccf797cf56f2d29050ae
zero <10652944+wafts@user.noreply.gitee.com>
2025-11-21 22:45:54 +0800

    优化tailwind.config.ts

--- nuxtjs/tailwind.config.ts
+++ nuxtjs/tailwind.config.ts
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './components/**/*.{vue,js,ts}',
-        './layouts/**/*.{vue,js,ts}',
-        './pages/**/*.{vue,js,ts}',
-        './plugins/**/*.{js,ts}'
+        './index.html',
+        './src/**/*.{html,js,ts,jsx,tsx,vue}'
     ],
-    darkMode: 'class',
     theme: {
         extend: {
             colors: {
-                'white': 'var(--color-white)',
-                'black': 'var(--color-black)',
-                'primary': {
+                white: 'var(--color-white)',
+                black: 'var(--color-black)',
+                body: 'var(--el-bg-color)',
+                page: 'var(--el-bg-color-page)',
+                light: 'var(--el-bg-color-light)',
+                lighte: 'var(--el-bg-color-lighter)',
+                primary: {
                     DEFAULT: 'var(--el-color-primary)',
                     'light-3': 'var(--el-color-primary-light-3)',
                     'light-5': 'var(--el-color-primary-light-5)',
@@ -21,40 +22,78 @@ export default {
                     'light-9': 'var(--el-color-primary-light-9)',
                     'dark-2': 'var(--el-color-primary-dark-2)'
                 },
-                'success': 'var(--el-color-success)',
-                'warning': 'var(--el-color-warning)',
-                'danger': 'var(--el-color-danger)',
-                'error': 'var(--el-color-error)',
-                'info': 'var(--el-color-info)',
-
-                'body': 'var(--el-bg-color)',
-                'page': 'var(--el-bg-color-page)',
-                'light': 'var(--el-bg-color-light)',
-                'lighter': 'var(--el-bg-color-lighter)',
-
-                'tx-primary': 'var(--el-text-color-primary)',
-                'tx-regular': 'var(--el-text-color-regular)',
-                'tx-secondary': 'var(--el-text-color-secondary)',
-                'tx-placeholder': 'var(--el-text-color-placeholder)',
-                'tx-disabled': 'var(--el-text-color-disabled)',
-
-                'br': 'var(--el-border-color)',
-                'br-dark': 'var(--el-border-color-dark)',
-                'br-darker': 'var(--el-border-color-darker)',
-                'br-light': 'var(--el-border-color-light)',
-                'br-lighter': 'var(--el-border-color-lighter)',
-                'br-extra-light': 'var(--el-border-color-extra-light)',
-
-                'fill': 'var(--el-fill-color)',
-                'fill-darker': 'var(--el-fill-color-darker)',
-                'fill-dark': 'var(--el-fill-color-dark)',
-                'fill-light': 'var(--el-fill-color-light)',
-                'fill-lighter': 'var(--el-fill-color-lighter)',
-                'fill-extra-light': 'var(--el-fill-color-extra-light)',
-                'fill-blank': 'var(--el-fill-color-blank)'
+                success: {
+                    DEFAULT: 'var(--el-color-success)',
+                    'light-3': 'var(--el-color-success-light-3)',
+                    'light-5': 'var(--el-color-success-light-5)',
+                    'light-7': 'var(--el-color-success-light-7)',
+                    'light-8': 'var(--el-color-success-light-8)',
+                    'light-9': 'var(--el-color-success-light-9)',
+                    'dark-2': 'var(--el-color-success-dark-2)'
+                },
+                warning: {
+                    DEFAULT: 'var(--el-color-warning)',
+                    'light-3': 'var(--el-color-warning-light-3)',
+                    'light-5': 'var(--el-color-warning-light-5)',
+                    'light-7': 'var(--el-color-warning-light-7)',
+                    'light-8': 'var(--el-color-warning-light-8)',
+                    'light-9': 'var(--el-color-warning-light-9)',
+                    'dark-2': 'var(--el-color-warning-dark-2)'
+                },
+                danger: {
+                    DEFAULT: 'var(--el-color-danger)',
+                    'light-3': 'var(--el-color-danger-light-3)',
+                    'light-5': 'var(--el-color-danger-light-5)',
+                    'light-7': 'var(--el-color-danger-light-7)',
+                    'light-8': 'var(--el-color-danger-light-8)',
+                    'light-9': 'var(--el-color-danger-light-9)',
+                    'dark-2': 'var(--el-color-danger-dark-2)'
+                },
+                error: {
+                    DEFAULT: 'var(--el-color-error)',
+                    'light-3': 'var(--el-color-error-light-3)',
+                    'light-5': 'var(--el-color-error-light-5)',
+                    'light-7': 'var(--el-color-error-light-7)',
+                    'light-8': 'var(--el-color-error-light-8)',
+                    'light-9': 'var(--el-color-error-light-9)',
+                    'dark-2': 'var(--el-color-error-dark-2)'
+                },
+                info: {
+                    DEFAULT: 'var(--el-color-info)',
+                    'light-3': 'var(--el-color-info-light-3)',
+                    'light-5': 'var(--el-color-info-light-5)',
+                    'light-7': 'var(--el-color-info-light-7)',
+                    'light-8': 'var(--el-color-info-light-8)',
+                    'light-9': 'var(--el-color-info-light-9)',
+                    'dark-2': 'var(--el-color-info-dark-2)'
+                },
+                tx: {
+                    primary: 'var(--el-text-color-primary)',
+                    regular: 'var(--el-text-color-regular)',
+                    secondary: 'var(--el-text-color-secondary)',
+                    placeholder: 'var(--el-text-color-placeholder)',
+                    disabled: 'var(--el-text-color-disabled)'
+                },
+                br: {
+                    DEFAULT: 'var(--el-border-color)',
+                    dark: 'var(--el-border-color-dark)',
+                    darker: 'var(--el-border-color-darker)',
+                    light: 'var(--el-border-color-light)',
+                    lighter: 'var(--el-border-color-lighter)',
+                    'extra-light': 'var(--el-border-color-extra-light)'
+                },
+                fill: {
+                    DEFAULT: 'var(--el-fill-color)',
+                    darker: 'var(--el-fill-color-darker)',
+                    dark: 'var(--el-fill-color-dark)',
+                    light: 'var(--el-fill-color-light)',
+                    lighter: 'var(--el-fill-color-lighter)',
+                    blank: 'var(--el-fill-color-blank)',
+                    'extra-light': 'var(--el-fill-color-extra-light)'
+                }
             },
             fontFamily: {
-                'sans': [
+                sans: [
                     'PingFang SC',
                     'Arial',
                     'Hiragino Sans GB',
@@ -63,11 +102,11 @@ export default {
                 ]
             },
             fontSize: {
-                'xs': 'var(--el-font-size-extra-small)',  // 12px
-                'sm': 'var(--el-font-size-small)',        // 13px
-                'base': 'var(--el-font-size-base)',       // 14px
-                'lg': 'var(--el-font-size-medium)',       // 16px
-                'xl': 'var(--el-font-size-large)',        // 18px
+                xs: 'var(--el-font-size-extra-small)',    // 12px
+                sm: 'var(--el-font-size-small)',          // 13px
+                base: 'var(--el-font-size-base)',         // 14px
+                lg: 'var(--el-font-size-medium)',         // 16px
+                xl: 'var(--el-font-size-large)',          // 18px
                 '2xl': 'var(--el-font-size-extra-large)', // 20px
                 '3xl': '22px',
                 '4xl': '24px',
@@ -120,10 +159,10 @@ export default {
                 96: '384px'
             },
             screens: {
-                'sm': '640px',
-                'md': '768px',
-                'lg': '1024px',
-                'xl': '1280px',
+                sm: '640px',
+                md: '768px',
+                lg: '1024px',
+                xl: '1280px',
                 '2xl': '1536px',
                 '3xl': '1792px',
                 '4xl': '2048px',
